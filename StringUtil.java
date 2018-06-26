132677fbf503e14cacc54c56832f2385055e2499
Evan D <evan.delasota@gmail.com>
2018-06-26 14:50:30 -0700

    Added 3 new methods: applyECDSASig(...) which applies an ECDSA signature, verifyECDSASig(...) which verifies a string signature, and getStringFromKey(...) which uses Base64 to encodes a byte array into a string.

--- StringUtil.java
+++ StringUtil.java
@@ -1,6 +1,10 @@
 package noobchain;
-
 import java.security.MessageDigest;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Key;
+import java.security.Signature;
+import java.util.Base64;
 
 public class StringUtil
 {
@@ -19,11 +23,57 @@ public class StringUtil
                 hexString.append(hex);
             }
             return hexString.toString();
+
         }
         catch (Exception e)
         {
             throw new RuntimeException(e);
         }
     }
+
+    // Applies ECDSA signature and returns the byte result.
+    public static byte[] applyECDSASig(PrivateKey privateKey, String input)
+    {
+        Signature dsa;
+        byte[] output = new byte[0];
+
+        try
+        {
+            dsa = Signature.getInstance("ECDSA", "BC");
+            dsa.initSign(privateKey);
+            byte[] strByte = input.getBytes();
+            dsa.update(strByte);
+            byte[] realSig = dsa.sign();
+            output = realSig;
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+        return output;
+    }
+
+    // Verifies a String signature.
+    public static boolean verifyECDSASig(PublicKey publicKey, String data, byte[] signature)
+    {
+        try
+        {
+            Signature ecdsaVerify = Signature.getInstance("ECDSA", "BC");
+            ecdsaVerify.initVerify(publicKey);
+            ecdsaVerify.update(data.getBytes());
+            return ecdsaVerify.verify(signature);
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static String getStringFromKey(Key key)
+    {
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
 }
 
