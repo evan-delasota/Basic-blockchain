a579709b1ce6b972f16e477c61ac9a50d82dcb4e
Evan D <evan.delasota@gmail.com>
2018-06-26 12:03:48 -0700

    Added a digital sig generator using SHA256

---
+++ StringUtil.java
@@ -0,0 +1,29 @@
+package noobchain;
+
+import java.security.MessageDigest;
+
+public class StringUtil
+{
+    public static String applySha256(String input)
+    {
+        try
+        {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            //Applying sha256 to the input...
+            byte[] hash = digest.digest(input.getBytes("UTF-8"));
+            StringBuffer hexString = new StringBuffer(); // Contains hash as a hexadecimal
+            for (int i = 0; i < hash.length; i++)
+            {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
