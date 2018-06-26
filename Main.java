32d409a5927741872ba450c69e6d7d1ec42c0580
Evan D <evan.delasota@gmail.com>
2018-06-26 13:05:11 -0700

    Imported GSON .jar lib, created an arrayList to store the blockchain in, and implemented a new method(isChainValid()) to check the integrity of the chain.

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
 package noobchain;
 
+import java.util.ArrayList;
+import com.google.gson.GsonBuilder;
+
 public class Main
 {
+    public static ArrayList<Blockchain> blockchain = new ArrayList<Blockchain>();
+
     public static void main(String[] args)
     {
         Blockchain genesisBlock = new Blockchain("Hi i'm the first block,", "0");
@@ -13,5 +18,31 @@ public class Main
         Blockchain thirdBlock = new Blockchain("Suh dude i'm the third block", secondBlock.hash);
         System.out.println("Hash for block 3: " + thirdBlock.hash);
 
+    }
+
+    public static Boolean isChainValid()
+    {
+        Blockchain currentBlock;
+        Blockchain previousBlock;
+
+        // Loops through blockchain to check hashes:
+        for (int i = 1; i < blockchain.size(); i++)
+        {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i - 1);
+            // Compare registered hash and calculated hash:
+            if (!currentBlock.hash.equals(currentBlock.calculateHash()))
+            {
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+            if (!previousBlock.hash.equals(currentBlock.previousHash))
+            {
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+        }
+
+        return true;
     }
 }
