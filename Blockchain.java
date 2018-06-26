ccde997cf2202c8e18a4915165bddc506176c2d6
Evan D <evan.delasota@gmail.com>
2018-06-26 13:31:32 -0700

    Added a new method mineBlock(...) to mine the block

--- Blockchain.java
+++ Blockchain.java
@@ -8,6 +8,7 @@ public class Blockchain
     public String previousHash;
     private String data; // Dummy data
     private long timeStamp;
+    private int nonce;
 
     public Blockchain(String data, String previousHash)
     {
@@ -20,12 +21,25 @@ public class Blockchain
     public String calculateHash()
     {
         String calculatedHash = StringUtil.applySha256(
-                              previousHash +
-                                    Long.toString(timeStamp) +
-                                    data
-                                    );
+                  previousHash +
+                        Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
+                        data
+                        );
+
         return calculatedHash;
     }
 
+    public void mineBlock(int difficulty)
+    {
+        // Creates a string with difficulty * "0".
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while (!hash.substring(0 , difficulty).equals(target))
+        {
+            nonce++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!: " + hash);
 
+    }
 }
